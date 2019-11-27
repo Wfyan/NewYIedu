22b81bf4f8b74a4befa4cbdfd05e300227208707
nothingyan <656276392@qq.com>
2019-11-27 11:33:09 +0800

    11/27

--- src/main/java/com/yi/mapper/KindInfoMapper.java
+++ src/main/java/com/yi/mapper/KindInfoMapper.java
@@ -2,9 +2,12 @@ package com.yi.mapper;
 
 import com.yi.entity.KindInfo;
 
+<<<<<<< HEAD
+=======
 import java.util.List;
 import java.util.Map;
 
+>>>>>>> ef0ae44e8159c38773805fbcabcf052098c8ca5f
 public interface KindInfoMapper {
     int deleteByPrimaryKey(Integer kid);
 
@@ -17,9 +20,14 @@ public interface KindInfoMapper {
     int updateByPrimaryKeySelective(KindInfo record);
 
     int updateByPrimaryKey(KindInfo record);
+<<<<<<< HEAD
+=======
 
     List<Integer> selectHigher(Integer level);
 
-    List<Map<String, Object>> selectAll(int higherId);
+    KindInfo selectByName(String name);
 
+    List<KindInfo> selectByHigherId(Integer higherId);
+
+>>>>>>> ef0ae44e8159c38773805fbcabcf052098c8ca5f
 }
