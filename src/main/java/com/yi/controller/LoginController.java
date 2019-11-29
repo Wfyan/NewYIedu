a8d281f4ed622d31843d3a76b3b5fa7e7349bbac
Wfy <13715075923@163.com>
2019-11-29 17:56:52 +0800

    11/29 Swagger

--- src/main/java/com/yi/controller/LoginController.java
+++ src/main/java/com/yi/controller/LoginController.java
@@ -4,12 +4,15 @@ import com.yi.service.ManagerLoginService;
 import com.yi.service.StudentLoginService;
 import com.yi.service.TeacherLoginService;
 import com.yi.util.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
+@Api(tags = "登录类")
 public class LoginController {
 
     @Autowired
@@ -25,6 +28,7 @@ public class LoginController {
      * @param password
      * @return
      */
+    @ApiOperation(value = "管理员登录",httpMethod = "GET",response = Result.class)
     @RequestMapping("/manager/goLogin")
     public Result authLogin(String name, String password) {
        return managerLoginService.goLogin(name,password);
@@ -37,6 +41,7 @@ public class LoginController {
      * @return
      */
     @RequestMapping("/stu/goLogin")
+    @ApiOperation(value = "学生登录",httpMethod = "GET",response = Result.class)
     public Result stuLogin(String phone, String password) {
         return studentLoginService.goLoginByPhone(phone,password);
     }
