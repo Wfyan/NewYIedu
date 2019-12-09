46c807a02d391ef516776ca76bc47cc4039c9223
Wfy <13715075923@163.com>
2019-12-09 17:59:38 +0800

    12/9整合完成

--- src/main/java/com/yi/service/impl/StudentLoginServiceImpl.java
+++ src/main/java/com/yi/service/impl/StudentLoginServiceImpl.java
@@ -1,16 +1,12 @@
 package com.yi.service.impl;
 
 import com.yi.dto.LoginOk;
-<<<<<<< HEAD
-import com.yi.entity.TbManager;
-import com.yi.mapper.TbManagerMapper;
-import com.yi.service.ManagerLoginService;
-=======
+
 import com.yi.entity.Permission;
 import com.yi.entity.TbStudent;
 import com.yi.mapper.PermissionMapper;
 import com.yi.mapper.TbStudentMapper;
->>>>>>> 49f47aeeb41f5292483bd371ccca8aa008c67c08
+
 import com.yi.service.StudentLoginService;
 import com.yi.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +16,14 @@ import java.util.List;
 
 @Service
 public class StudentLoginServiceImpl implements StudentLoginService {
-<<<<<<< HEAD
-=======
+
     @Autowired
     private TbStudentMapper studentMapper;
->>>>>>> 49f47aeeb41f5292483bd371ccca8aa008c67c08
     @Autowired
     private PermissionMapper mapper;
 
     @Override
-<<<<<<< HEAD
-    public Result goLoginByName(String name, String password) {
-        return null;
-    }
 
-    @Override
-    public Result goLoginByPhone(String phone, String password) {
-        return null;
-=======
     public Result goLoginByPhone(String phone, String password) {
         TbStudent student = studentMapper.selectByPhone(phone);
         if(student == null){
@@ -47,7 +33,7 @@ public class StudentLoginServiceImpl implements StudentLoginService {
             return new Result().successMessage("登录成功！",loginOk(student,mapper.selectByLevel(3)));
         }
         return new Result().error("账号密码错误！");
->>>>>>> 49f47aeeb41f5292483bd371ccca8aa008c67c08
+
     }
     private static LoginOk loginOk(TbStudent student, List<Permission> permissions){
         LoginOk loginOk =  new LoginOk();
